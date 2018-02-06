6b19daeaaf7ce343d42644143ca3b5d3fcb1603e
Srinivas Polaji <spolaji@gmail.com>
2018-02-05 22:48:02 -0500

    week4 changes.

--- src/textgen/MarkovTextGeneratorLoL.java
+++ src/textgen/MarkovTextGeneratorLoL.java
@@ -29,10 +29,42 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 	
 	
 	/** Train the generator by adding the sourceText */
+	@SuppressWarnings("unlikely-arg-type")
 	@Override
 	public void train(String sourceText)
 	{
 		// TODO: Implement this method
+		String[] words = sourceText.split(" ");
+		
+		for(String s : words) {
+			System.out.println("Word: " +s);
+		}
+		
+		starter = words[0];
+		String prevWord = starter;
+		for(int i = 1; i < words.length; i++) {
+			if(wordList.contains(prevWord)) {
+				ListNode oldNode = wordList.get(wordList.indexOf(prevWord));
+				oldNode.addNextWord(words[i]);
+			} else {
+				ListNode newNode = new ListNode(prevWord);
+				newNode.addNextWord(words[i]);
+				wordList.add(newNode);
+			}	
+			prevWord = words[i];
+			
+			if(i == words.length -1) {
+				if(wordList.contains(prevWord)) {
+					ListNode oldNode = wordList.get(wordList.indexOf(prevWord));
+					oldNode.addNextWord(words[0]);
+				} else {
+					ListNode newNode = new ListNode(prevWord);
+					newNode.addNextWord(words[0]);
+					wordList.add(newNode);
+				}
+					
+			}
+		}
 	}
 	
 	/** 
@@ -41,7 +73,15 @@ public class MarkovTextGeneratorLoL implements MarkovTextGenerator {
 	@Override
 	public String generateText(int numWords) {
 	    // TODO: Implement this method
-		return null;
+		StringBuilder sb = new StringBuilder();
+		Random generator = new Random();
+		String word = "";
+		for(int i = 0; i <= numWords; i++) {
+			ListNode currWord = wordList.get(i);
+			word = currWord.getRandomNextWord(generator);
+			sb.append(word);
+		}
+		return sb.toString();
 	}
 	
 	
@@ -144,9 +184,10 @@ class ListNode
 		// TODO: Implement this method
 	    // The random number generator should be passed from 
 	    // the MarkovTextGeneratorLoL class
-	    return null;
+	    int randomInt = generator.nextInt(nextWords.size()-1);
+		return nextWords.get(randomInt);
 	}
-
+	
 	public String toString()
 	{
 		String toReturn = word + ": ";
